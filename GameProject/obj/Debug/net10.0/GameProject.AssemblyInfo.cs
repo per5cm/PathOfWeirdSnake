535f9f392ab5b9a38763225888feac19351a95bb
per5cm <per5cm@proton.me>
2026-04-07 22:50:08 +0200

    insider stuff

--- GameProject/obj/Debug/net10.0/GameProject.AssemblyInfo.cs
+++ GameProject/obj/Debug/net10.0/GameProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0994aed92d9b1cf29da512c623c591e011b33817")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d55b46c155700185b5ed8272d5bfe01e4ed95287")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
