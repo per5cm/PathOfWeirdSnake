8b2f74a071714e2a6d8c1507e91bb29497c59387
Alchemist <per5cm@proton.me>
2026-09-01 09:12:44 +0200

    g

--- GameProject/obj/Debug/net10.0/GameProject.AssemblyInfo.cs
+++ GameProject/obj/Debug/net10.0/GameProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a931f4204dd647db290167d528b0a85dd240c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b7eb873661a980bcc9d9e135d9243145267c57")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
